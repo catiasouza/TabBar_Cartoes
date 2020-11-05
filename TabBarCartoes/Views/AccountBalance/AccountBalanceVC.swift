14c3a281daa4183b15d3e62d72ffd45fb8f7d9bc
Catia <catiamsouza80@gmail.com>
2020-11-04 21:36:19 -0300

    aplicando o MVC

--- TabBarCartoes/Views/AccountBalance/AccountBalanceVC.swift
+++ TabBarCartoes/Views/AccountBalance/AccountBalanceVC.swift
@@ -7,28 +7,16 @@
 
 import UIKit
 
-class AccountBalanceVC: UIViewController {
+class AccountBalanceController{
     
-    @IBOutlet weak var timeLine: UITableView!
+    private var arrayLancamentos:[LancamentoElements] = []
+    private var currentLancamentoElement: LancamentoElements?
     
-  
-    var arrayLancamentos:[LancamentoElements] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.arrayLancamentos = self.loadMovimentacoes() ?? []
-
-        self.timeLine.register(UINib(nibName: "ExtratoCell", bundle: nil), forCellReuseIdentifier: "ExtratoCell")
-      
-        print("Second----viewDidLoad")
-        self.timeLine.delegate = self
-        self.timeLine.dataSource = self
-        self.timeLine.separatorStyle = .none
-        
+    func loadCurrentLancamentoElement(index: Int){
+        self.currentLancamentoElement = arrayLancamentos[index]
     }
     
-    private func loadMovimentacoes() -> [LancamentoElements]?{
+    func loadLancamentos() {
         
         if let path = Bundle.main.path(forResource: "despesas", ofType: "json"){
             
@@ -38,27 +26,52 @@ class AccountBalanceVC: UIViewController {
                 let movimentacao = try JSONDecoder().decode(Movimentacao.self, from: data)
                 
                 print("====>> Movimentacao \(movimentacao)")
-                return movimentacao.lancamentos
+                self.arrayLancamentos =      movimentacao.lancamentos
             }catch{
                 print("=== >>>> Deu ruim no parse")
-                return nil
             }
         }
-        return nil
     }
+    var numberOfRows: Int {
+        return self.arrayLancamentos.count
+    }
+    var lancamentoElement: LancamentoElements?{
+        return self.currentLancamentoElement
+    }
+    
+}
+class AccountBalanceVC: UIViewController {
+    
+    @IBOutlet weak var timeLine: UITableView!
+    
+    private var controller: AccountBalanceController = AccountBalanceController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.timeLine.register(UINib(nibName: "ExtratoCell", bundle: nil), forCellReuseIdentifier: "ExtratoCell")
+        
+        self.controller.loadLancamentos()
+        
+        self.timeLine.delegate = self
+        self.timeLine.dataSource = self
+        self.timeLine.separatorStyle = .none
+    }
+    
 }
 
 extension AccountBalanceVC: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return self.arrayLancamentos.count
+        self.controller.numberOfRows
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell: ExtratoCell? = tableView.dequeueReusableCell(withIdentifier: "ExtratoCell", for: indexPath) as? ExtratoCell
-        cell?.setup(value: self.arrayLancamentos[indexPath.row])
+        self.controller.loadCurrentLancamentoElement(index: indexPath.row)
+        
+        cell?.setup(value: self.controller.lancamentoElement)
         return cell ?? UITableViewCell()
     }
     
